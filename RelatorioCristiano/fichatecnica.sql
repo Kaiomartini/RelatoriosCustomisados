c311e02b9a3c26814481dfb776a60ed4804175d8
Caio Martini <42272355+Kaiomartini@users.noreply.github.com>
2022-10-25 15:11:16 -0300

    updade
    relatorio

--- RelatorioCristiano/fichatecnica.sql
+++ RelatorioCristiano/fichatecnica.sql
@@ -6,9 +6,7 @@ FUNCTION DGEFR_FichaTecnicaProduto(nSeqProduto in DGE_PRODUTO.SeqProduto%Type,
     sRazaoSocial        GE_Empresa.RazaoSocial%Type;
     vLogo               BLOB := NULL;
     vDataAtual          varchar2(10) := TO_CHAR(SYSDATE, 'DD/MM/YYYY'); 
-    
 -- Variaveis cidade inicio
-
     vRazaosocial        GE_Empresa.RAZAOSOCIAL%Type := null;
     vNomeFantasia       GE_Empresa.FANTASIA%Type := null;
     vCNPJ1              GE_Empresa.CNPJENTIDADE%Type := null;
@@ -22,7 +20,6 @@ FUNCTION DGEFR_FichaTecnicaProduto(nSeqProduto in DGE_PRODUTO.SeqProduto%Type,
     vIE                 GE_Empresa.inscrestadual%Type := null;
 
 -- FICHA TECNICA do produto
-
    vCodSif              varchar2(500);
    vGtinUnidadePadrao   varchar2(500);
    vGtinMenorControle   varchar2(500);
@@ -674,13 +671,13 @@ cHTML := cHTML ||'
 
         
         <div class="row mx-0">
-            <div class="col distaca text-center fw-bold border border-white fs-5">Embalagem CX(1)</div>
+            <div class="col distaca text-center fw-bold border border-white fs-5">Embalagem</div>
         </div><!--fim row-->
              
-        <div class="row row-cols-5 mb-0 mx-0"> <!--informações gerais padrao 2 com distaca -->
+        <div class="row row-cols-5 mb-0 mx-0"><!-- informações gerais padrao 2 com distaca -->
               
            <div class="col distaca text-center  px-1 border border-white">
-               <div class="fw-bold ">cod gtin </div>
+               <div class="fw-bold ">cod gtin</div>
                <div class="bg-white mb-1 ">
                    '||vGtinMenorControle||'
                </div>
@@ -790,11 +787,11 @@ cHTML := cHTML ||'
             </div>
         </div><!-- #################### --- FIM tabela  ENBALAGEM PRIMARIA -- ################################  -->
        
-        <div id="embalagem" class="row">  <!-- #################### --- FIM tabela ENBALAGEM SECUNDARIA-- ################################  -->
+        <div id="embalagem" class="row">  <!-- #################### --- inicio tabela ENBALAGEM SECUNDARIA-- ################################  -->
             <div class="col-12">
                 <div class="caixa">
                     <div class="distaca fs-5 fw-bold text-center">
-                        Insumos para cadastrar a tara da embalagem primária
+                        Insumos para cadastrar a tara da embalagem secundaria
                     </div>
                     <div class="row">
                         <div class="mx-auto col-12">
@@ -1027,18 +1024,11 @@ cHTML := cHTML ||'
     </div><!--fim A4 Page3-->
     <div id="Page4"class="A4">
         <!-- #################### --- FIM bloco 10 -- ################################  -->
-        
+   
         <br />
         <!-- #################### --- FIM bloco 10 -- ################################  -->
-
-
-
-
     </div><!--fim A4 Page4-->
-
-
-
-
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 
